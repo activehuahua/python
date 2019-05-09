3528bf19c4c82414fcc4328457176750a8ce1886
zhaojianghua <zhaojianghua@pretang.com>
2019-05-09 14:45:20 +0800

    提交前端接口代码

--- pythonProject/seleniumTest/bigCommerceLogin.py
+++ pythonProject/seleniumTest/bigCommerceLogin.py
@@ -20,9 +20,9 @@ options.add_argument('user-agent="Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like M
     # 'Accept:"application/json, text/plain, */*"',
     # 'Host:"backendappaws.bundleb2b.net"'
                      )
-#browser = webdriver.Chrome(options=options)
+browser = webdriver.Chrome(options=options)
 
-browser=webdriver.Firefox()
+#browser=webdriver.Firefox()
 browser.implicitly_wait(10)
 browser.get(login_url)
 
@@ -40,18 +40,18 @@ browser.find_element_by_xpath('//div[@id="stores"]/a[2]').click()
 
 api_url='https://backendappaws.bundleb2b.net/api/companies'
 
-# header={
-#       'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
-#     'Referer':'https://backendappaws.bundleb2b.net/companies',
-#     'Accept':'application/json, text/plain, */*',
-#     'Host':'backendappaws.bundleb2b.net',
-#     }
+header={
+      'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
+    'Referer':'https://backendappaws.bundleb2b.net/companies',
+    'Accept':'application/json, text/plain, */*',
+    'Host':'backendappaws.bundleb2b.net',
+    }
 
-cookies=browser.get_cookies()
-
-
-cookies = {v.get("name"):v.get("value") for v in cookies}
-print(type(cookies),cookies)
+# cookies=browser.get_cookies()
+#
+#
+# cookies = {v.get("name"):v.get("value") for v in cookies}
+# print(type(cookies),cookies)
 
 # response=requests.get(api_url,headers=header,cookies=cookies)
 # #print(response.text)
@@ -60,10 +60,23 @@ print(type(cookies),cookies)
 #response.json()
 #print(type(response),response,response.text)
 
-app=browser.find_element_by_xpath('//div[@class="nav-apps"]/a')
+app=browser.find_element_by_xpath('//div[@id="nav-apps"]')
 app.click()
 
+sleep(3)
 
+browser.delete_all_cookies()
+browser.refresh()
+
+store_link=browser.find_element_by_xpath('//ul[@class="cp-nav-group-content"]/li/a[@href="/manage/app/13706"]')
+store_link.click()
+
+cookies=browser.get_cookies()
+cookies = {v.get("name"):v.get("value") for v in cookies}
+print(type(cookies),cookies)
+
+response=requests.get(api_url,headers=header,cookies=cookies)
+print(response.text)
 
 res=browser.get(api_url)
 print(type(res),res)
