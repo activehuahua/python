59c872c3106c3718a21d5af43b5ad2ee2a8e68b4
activehuahua <164033495@qq.com>
2019-08-22 17:43:46 +0800

    自动统计BUG信息，发送邮件

--- SilkTools/redmine-HM/config.py
+++ SilkTools/redmine-HM/config.py
@@ -15,17 +15,36 @@ urgent_url = '?utf8=%E2%9C%93&set_filter=1&f%5B%5D=status_id&op%5Bstatus_id%5D=o
 immediate_url = '?utf8=✓&set_filter=1&f%5B%5D=status_id&op%5Bstatus_id%5D=o&f%5B%5D=priority_id&op%5Bpriority_id%5D=%3D&v%5Bpriority_id%5D%5B%5D=5&f%5B%5D=&c%5B%5D=tracker&c%5B%5D=status&c%5B%5D=priority&c%5B%5D=subject&c%5B%5D=assigned_to&c%5B%5D=updated_on&c%5B%5D=due_date&c%5B%5D=fixed_version&c%5B%5D=cf_4&group_by='
 
 UATData = {
-    'UAT Low:': uat_url + low_url,
-    'UAT Normal:': uat_url + normal_url,
-    'UAT High:': uat_url + high_url,
-    'UAT Urgent:': uat_url + urgent_url,
-    'UAT Immediate': uat_url + immediate_url
+    'Low': uat_url + low_url,
+    'Normal': uat_url + normal_url,
+    'High': uat_url + high_url,
+    'Urgent': uat_url + urgent_url,
+    'Immediate': uat_url + immediate_url
 }
 
 AppData = {
-    'APP Low:': app_url + low_url,
-    'APP Normal:': app_url + normal_url,
-    'APP High:': app_url + high_url,
-    'APP Urgent:': app_url + urgent_url,
-    'APP Immediate': app_url + immediate_url
+    'Low': app_url + low_url,
+    'Normal': app_url + normal_url,
+    'High': app_url + high_url,
+    'Urgent': app_url + urgent_url,
+    'Immediate': app_url + immediate_url
 }
+
+UATList= {
+    'Low': 1,
+    'Normal': 0,
+    'High': 0,
+    'Urgent': 0,
+    'Immediate': 0
+}
+APPList= {
+    'Low': 0,
+    'Normal': 0,
+    'High': 0,
+    'Urgent': 0,
+    'Immediate': 0
+}
+
+filepath=r'D:\\Alex\\SilkSoftware\\项目\\H&M\\测试文档\\BUG统计\\'
+
+sendTo=  ['alexander.zhao@silksoftware.com','164033495@qq.com']
--- SilkTools/redmine-HM/seleniumRedmine.py
+++ SilkTools/redmine-HM/seleniumRedmine.py
@@ -5,35 +5,51 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re, string
 import config as CF
+import xlwt
+from selenium.webdriver.chrome.options import Options
+import xlrd
+from openpyxl import load_workbook
+import time
+import xlsxwriter
+from xlutils.copy import copy
+from sendReport import SENDMAIL
+
+import xlutils, openpyxl
 
 
 class SeleniumRedmine(object):
 
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver.get(CF.base_url)
 
         self.driver.find_element_by_id('username').send_keys('alexander.zhao')
         self.driver.find_element_by_id('password').send_keys('Mi123456')
         self.driver.find_element_by_xpath('//input[@type="submit"]').click()
-       # self.driver.get(CF.uat_url)
 
     def getEachNumber(self):
         for key in CF.UATData:
             self.driver.get(CF.UATData[key])
             number = self.driver.find_element_by_xpath('//p[@class="pagination"]/span[@class="items"]').text
-            print(key, self.getNumber(number))
+            num = self.getNumber(number)
+            CF.UATList[key] = num
+            print(key, num)
 
     def getAppEachNumber(self):
         for key in CF.AppData:
             self.driver.get(CF.AppData[key])
             try:
                 number = self.driver.find_element_by_xpath('//p[@class="pagination"]/span[@class="items"]').text
-                print(key, self.getNumber(number))
+                num = self.getNumber(number)
+                CF.APPList[key] = num
+                print(key, num)
             except:
                 pass
 
-    def getNumber(self,content):
+    def getNumber(self, content):
         number = content.split('/')[1]
         number = number.replace(')', '')
         return number
@@ -41,10 +57,44 @@ class SeleniumRedmine(object):
     def quit(self):
         self.driver.quit()
 
+    def writeToExcel(self):
+        file = CF.filepath + r'HM-BUG统计-.xlsx'
+        _date = time.strftime('%Y%m%d', time.localtime(time.time()))
+        print(_date)
+        newfile = 'HM-BUG统计-' + _date + '.xlsx'
+        print(newfile)
+
+        workbook = openpyxl.load_workbook(file)
+        sheet = workbook.active
+
+        # 写入UAT数据
+        sheet.cell(2, 2, int(CF.UATList['Low']))
+        sheet.cell(3, 2, int(CF.UATList['Normal']))
+        sheet.cell(4, 2, int(CF.UATList['High']))
+        sheet.cell(5, 2, int(CF.UATList['Urgent']))
+        sheet.cell(6, 2, int(CF.UATList['Immediate']))
+
+        # 写入App数据
+
+        sheet.cell(25, 2, int(CF.APPList['Low']))
+        sheet.cell(26, 2, int(CF.APPList['Normal']))
+        sheet.cell(27, 2, int(CF.APPList['High']))
+        sheet.cell(28, 2, int(CF.APPList['Urgent']))
+        sheet.cell(29, 2, int(CF.APPList['Immediate']))
+
+        newfile = CF.filepath + newfile
+        workbook.save(newfile)
+
+        SENDMAIL(CF.sendTo, newfile)
+
 
 if __name__ == '__main__':
     redmine = SeleniumRedmine()
     redmine.getEachNumber()
     print('***********************************')
     redmine.getAppEachNumber()
+    print(CF.UATList)
+    print(CF.APPList)
     redmine.quit()
+
+    redmine.writeToExcel()
