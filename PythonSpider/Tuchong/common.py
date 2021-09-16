64fefa36c43051883c514df30f783e540f8a579b
activehuahua <cdzjh730815>
2021-09-16 23:37:44 +0800

    提交文档

--- PythonSpider/Tuchong/common.py
+++ PythonSpider/Tuchong/common.py
@@ -17,7 +17,33 @@ class common:
         return topicId
 
 
+    def getResult():
+        goods=r"goods=[{'name':'alex','age':'20'},{'name':'bob','age':'30'}];"
+        result=[{'name':'alex','age':'20'},{'name':'bob','age':'30'}]
+        print(result)
+        print(len(result))
+        print(type(result))
+        for i in range(len(result)):
+            print(result[i])
+
+        goods=goods.replace(';','')
+        print(goods)
+        pattern = re.compile('goods=(.*)', re.S)
+        # result=re.findall(pattern,goods)
+        result=pattern.findall(goods)
+
+
+        result=list(result)
+
+        print(len(result))
+        print(type(result))
+        for i in range(len(result)):
+            print(result[i])
+
+
 if __name__ == '__main__':
     url='https://stock.tuchong.com/topic?topicId=50064'
     topicId=common.getTopicId(url)
-    print(topicId)
+    common.getResult()
+
+
