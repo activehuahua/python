cb4c6c9f87acb33c6355fe895e129449f33c55cf
alexander.zhao <Mi123456>
2021-09-17 17:15:52 +0800

    修改代码

--- PythonSpider/Tuchong/common.py
+++ PythonSpider/Tuchong/common.py
@@ -7,7 +7,7 @@
 # Author:       alex
 # Date:         2021/9/16
 '''
-import re
+import re,json
 
 class common:
     def getTopicId(url):
@@ -16,8 +16,29 @@ class common:
         topicId=result.group(1)
         return topicId
 
+    def getResult():
+        goods = r"goods=[{'name':'alex','age':false},{'name':'bob','age':False}];"
+        result = [{'name': 'alex', 'age': '20'}, {'name': 'bob', 'age': '30'}]
+
+        print(goods)
+        pattern = re.compile(r"goods=\[(.*)\];", re.S)
+        result = pattern.findall(goods)
+        str=result[0]
+        str=str.replace('false','False')
+
+        result=list(eval(result[0].replace('false','False')))
+        print(result.__len__())
+
+    def getResult1():
+        goods = r"goods=[{'name':'alex','age':'20'},{'name':'bob','age':'30'}]"
+        result = [{'name': 'alex', 'age': '20'}, {'name': 'bob', 'age': '30'}]
+
+        print(goods)
+        pattern = re.compile(r"'name':(.*?),", re.S)
+        result = pattern.findall(goods)
 
 if __name__ == '__main__':
     url='https://stock.tuchong.com/topic?topicId=50064'
     topicId=common.getTopicId(url)
-    print(topicId)
+    # print(topicId)
+    common.getResult()
